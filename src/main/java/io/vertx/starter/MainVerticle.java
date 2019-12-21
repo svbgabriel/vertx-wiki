a9e69301586ab91b3b0c61174dbf8669a43a8341
Gabriel Batista <svbgabriel@gmail.com>
2019-12-21 15:47:14 -0300

    Implementa a atualização de páginas

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -177,6 +177,38 @@ public class MainVerticle extends AbstractVerticle {
 		context.response().end();
 	}
 
+	private void pageUpdateHandler(RoutingContext context) {
+		String id = context.request().getParam("id");
+		String title = context.request().getParam("title");
+		String markdown = context.request().getParam("markdown");
+		boolean newPage = "yes".equals(context.request().getParam("newPage"));
+
+		dbClient.getConnection(car -> {
+			if (car.succeeded()) {
+				SQLConnection connection = car.result();
+				String sql = newPage ? SQL_CREATE_PAGE : SQL_SAVE_PAGE;
+				JsonArray params = new JsonArray();
+				if (newPage) {
+					params.add(title).add(markdown);
+				} else {
+					params.add(markdown).add(id);
+				}
+				connection.updateWithParams(sql, params, res -> {
+					connection.close();
+					if (res.succeeded()) {
+						context.response().setStatusCode(303);
+						context.response().putHeader("Location", "/wiki/" + title);
+						context.response().end();
+					} else {
+						context.fail(res.cause());
+					}
+				});
+			} else {
+				context.fail(car.cause());
+			}
+		});
+	}
+
 	@Override
 	public void start(Promise<Void> promise) {
 		Future<Void> steps = prepareDatabase().compose(v -> startHttpServer());
